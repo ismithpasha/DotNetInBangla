3c8e6b510a7cc80e76bc44b9981f0fb56c9d8b84
pasha <pasha@trustaxiatapay.com>
2025-04-27 12:07:35 +0600

    Chapter one A completed

--- ExamplesByChapter/Chapter01/Chapter01A/Chapter01A/Program.cs
+++ ExamplesByChapter/Chapter01/Chapter01A/Chapter01A/Program.cs
@@ -2,4 +2,22 @@
 using System.Text;
 
 Console.OutputEncoding = Encoding.UTF8;
-Console.WriteLine(" স্বাগতম C# প্রোগ্রামিং-এ!");
+Console.WriteLine("স্বাগতম C# প্রোগ্রামিং-এ!");
+
+
+/*
+ * Older style (version < .Net 5)
+ * 
+using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("স্বাগতম C# প্রোগ্রামিং-এ!");
+    }
+}
+
+ * 
+ */
